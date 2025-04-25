c44f598416c66983731caa18be847a5646aecace
MSI\StormSpirit <523810185@qq.com>
2025-04-26 03:13:24 +0800

    提交ignore之后的文件

---
+++ CodeLin/Source/CodeLin/CodeLinGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CodeLinGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACodeLinGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACodeLinGameMode();
+};
+
+
+
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,2 +1,4 @@
 2025.4.24，文件夹起好了！
 尝试提交一下！
+git ignore 相关配置：https://gitcode.com/gh_mirrors/ue/ue5-gitignore/?utm_source=artical_gitcode&index=top&type=href&
+git lfs pull
